4a7494908b49ed6b61fbff4c58c896ce513eb230
Alexandre DU <dualexandre2004@gmail.com>
2024-11-04 10:57:43 +0100

    test

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>title</title>
+    <link href = "style.css" rel = "stylesheet">
+</head>
+<body>
+
+<header>
+
+<h1>
+    Collection de film
+</h1>
+
+<nav>
+    <ul>
+        <li>
+            <a href="#" class ="link">Vos Films</a>
+        </li>
+        <li>
+            <a href="#" class ="link">Ajouter Des Films</a>
+        </li>
+        <li>
+            <a href="#" class ="link">Modifier Vos Films</a>
+        </li>
+    </ul>
+   
+</nav>
+
+</header>
+
+
+<main>
+
+<section id = "catalogue">
+    
+    <div class = "box">
+        <h1 class = "titreFilm">Test</h1>
+    <form class = "avis">
+        <label for = "avis">Laissez votre avis</label>
+        <input type="text" name="avis" id="avis" placeholder = "Avis">
+        <button class = "btn" type="submit" value="Envoyer">Envoyer</button>
+    </form>
+    </div>
+
+</section>
+
+</main>
+
+<footer>
+
+</footer>
+
+<?php
+
+
+
+
+
+?>
+    
+</body>
+</html>
+
