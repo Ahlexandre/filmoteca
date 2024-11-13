9a876fa3f312ed0bc95d3d1d0fe6ee85a9cc087a
Alexandre DU <dualexandre2004@gmail.com>
2024-11-13 12:22:28 +0100

    version 1.0.0

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-include "connexion.php";
+
+include 'connexion.php';
+
+
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+}
+
+if ($page === 'contact') {
+    echo "<h1>Page de Contact</h1>";
+    var_dump("Vous êtes sur la page de contact");
+} elseif ($page === 'films') {
+    echo "<h1>Page des Films</h1>";
+    var_dump("Vous êtes sur la page des films");
+} else {
+    echo "<h1>Page d'accueil</h1>";
+    var_dump("Bienvenue sur la page d'accueil");
+}
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +24,7 @@ include "connexion.php";
 <head>
     <meta charset="UTF-8" />
     <title>title</title>
-    <link href = "styles/style.css" rel = "stylesheet">
+    <link href="styles/style.css" rel="stylesheet">
 </head>
 <body>
 
@@ -20,46 +37,53 @@ include "connexion.php";
 <nav>
     <ul>
         <li>
-            <a href="#" class ="link">Vos Films</a>
+            <a href="index.php?page=films" class="link">Vos Films</a>
         </li>
         <li>
-            <a href="ajouterFilm.php" class ="link">Ajouter Des Films</a>
+            <a href="ajouterFilm.php" class="link">Ajouter Des Films</a>
         </li>
         <li>
-            <a href="modifierFilm.php" class ="link">Modifier Vos Films</a>
+            <a href="modifierFilm.php" class="link">Modifier Vos Films</a>
+        </li>
+        <li>
+            <a href="index.php?page=films" class="Films">Films</a>
+        </li>
+        <li>
+            <a href="index.php?page=contact" class="link">Contact</a>
         </li>
     </ul>
-   
+
 </nav>
 </header>
 
 
 <main>
-<section id = "catalogue">
+<section id="catalogue">
 
 <?php
+    // Simulation d'une requête pour afficher des films
     $sql = "SELECT * FROM movie";
     $resultat = $conn->query($sql);
-    if(mysqli_num_rows($resultat)>0){
-        while($listeFilm = mysqli_fetch_assoc($resultat)){
+    if (mysqli_num_rows($resultat) > 0) {
+        while ($listeFilm = mysqli_fetch_assoc($resultat)) {
 ?>
-    <a target = "_BLANK" href="page.php?id=<?php echo $listeFilm['id']; ?>">
-    <div class = "box">
+    <a target="_BLANK" href="page.php?id=<?php echo $listeFilm['id']; ?>">
+    <div class="box">
         <table>
             <tr>
-                <th> <h1 class = "titreFilm"><?php echo $listeFilm['title']; ?></h1></th>
+                <th> <h1 class="titreFilm"><?php echo $listeFilm['title']; ?></h1></th>
             </tr>
             <tr>
-                <td> Date de sortie : <?php echo $listeFilm['year']?></td>
+                <td> Date de sortie : <?php echo $listeFilm['year']; ?></td>
             </tr>
             <tr>
-                <td> Synopsis : <?php echo $listeFilm['synopsis']?></td>
+                <td> Synopsis : <?php echo $listeFilm['synopsis']; ?></td>
             </tr>
             <tr>
-                <td> Réalisateur : <?php echo $listeFilm['director']?></td>
+                <td> Réalisateur : <?php echo $listeFilm['director']; ?></td>
             </tr>
             <tr>
-                <td> Genre : <?php echo $listeFilm['genre']?></td>
+                <td> Genre : <?php echo $listeFilm['genre']; ?></td>
             </tr>
         </table>
         
@@ -69,17 +93,15 @@ include "connexion.php";
     </a>
 <?php
         }
-    }else{
+    } else {
         echo "Vous n'avez aucun film";
     }
 ?>
 
 <?php
-
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $avis = $_POST["avis"];
-
-    $sql = "INSERT INTO avis(id,avis) VALUES('$id', '$avis')";
+    $sql = "INSERT INTO avis(id, avis) VALUES('$id', '$avis')";
 }
 ?>
 
@@ -92,13 +114,7 @@ if(isset($_POST["submit"])){
 </footer>
 
 <?php
-
-
-
-
-
 ?>
-    
+
 </body>
 </html>
-
