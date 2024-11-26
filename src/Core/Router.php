409169daf3696cef9cee47f486a571ec82b7cd44
Alexandre DU <dualexandre2004@gmail.com>
2024-11-26 22:37:50 +0100

    Mise à jour des Controlleur (Front & Film Controller) et Création de Service

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -1,20 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\Core;
+
 class Router
 {
-    public function route()
+    public function route(): void
     {
+        /**
+         * echo '<pre>';
+         * var_dump($_SERVER);
+         * echo '</pre>';die;
+         */
+
         // Récupère l'URL demandée (sans le domaine et la racine)
         $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-        
-        // Découpe l'URI pour obtenir la route et l'action
-        $parts = explode('/', $uri);
-        // Exemple: ['films', 'create']
-        $route = $parts[0] ?? null; // 'films'
-        $action = $parts[1] ?? null; // 'create'
 
-        // Récupère les paramètres de la query string (comme ?id=126&name=Inception)
-        $queryParams = $_GET;
+        // Découpe l'URI pour obtenir la route et l'action
+        $parts = explode('/', $uri); // Exemple : ['films', 'create']
+
+        $route = $parts[0] ?? null;   // 'films'
+        $action = $parts[1] ?? null; // 'create'
 
         // Définit les routes et leurs contrôleurs associés
         $routes = [
@@ -25,6 +32,7 @@ class Router
         if (array_key_exists($route, $routes)) {
             // Crée dynamiquement le contrôleur
             $controllerName = 'App\\Controller\\' . $routes[$route];
+
             if (!class_exists($controllerName)) {
                 echo "Controller '$controllerName' not found";
                 return;
@@ -32,10 +40,11 @@ class Router
 
             $controller = new $controllerName();
 
+
             // Vérifie si la méthode existe dans le contrôleur
             if (method_exists($controller, $action)) {
-                // Appelle la méthode dynamique
-                $controller->$action($queryParams);
+                $queryParams = $_GET; // Récupère les paramètres éventuels
+                $controller->$action($queryParams); // Appelle la méthode dynamique correspondant à l'action du contrôleur
             } else {
                 echo "Action '$action' not found in $controllerName";
             }
